5307ef6b74b8998130dbf14347dc749a09e86d65
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-06 21:36:14 +0300

    Add home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import Movies from '../components/Movies'
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <Movies></Movies>
+        </div>
+    );
+}
+
+export default HomePage;

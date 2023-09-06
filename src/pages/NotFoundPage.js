867216f27ea323f324b1dfe50b5a0163994e92d3
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-06 21:37:03 +0300

    Add not found page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+
+function NotFoundPage() {
+    return (
+        <Container className="NotFoundPage">
+            NotFoundPage
+        </Container>
+    );
+}
+
+export default NotFoundPage;
